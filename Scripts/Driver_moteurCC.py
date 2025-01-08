fba2ee777ae9ab95632367ea005587f0447a1743
gildeb <48280441+gildeb@users.noreply.github.com>
2025-01-08 16:31:50 +0100

    Add files via upload

--- Scripts/Driver_moteurCC.py
+++ Scripts/Driver_moteurCC.py
@@ -3,15 +3,19 @@ from machine import Pin, PWM
 class moteurCC:
 
     def __init__(self, pin1=0, pin2=1, en=2, freq=1000):
-        self.ina =
-        self.inb =
-        self.en  =
+        self.ina = Pin(pin1, Pin.OUT)
+        self.inb = Pin(pin2, Pin.OUT)
+        self.en  = PWM(en, freq=1000, duty=0)
 
-    def avant(self, vit):
-        ''' A compléter'''
+    def avant(self, vit):   # vit=duty, entre 0 et 1023
+        self.ina.on()
+        self.inb.off()
+        self.en.duty(vit)
 
-    def arriere(self, vit):
-        ''' A compléter'''
+    def arriere(self, vit): # vit=duty, entre 0 et 1023
+        self.ina.off()
+        self.inb.on()
+        self.en.duty(vit)
 
     def stop(self):
-        ''' A compléter'''
+        self.en.duty(0)
